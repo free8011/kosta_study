5f427bb6b3aa11a8ab2d96a7b4929066b0a66b08
hyunil chon <free8011@naver.com>
2019-07-11 17:18:20 +0900

    Update KostascmApplication.java

--- KOSTASCM/src/main/java/com/example/KostascmApplication.java
+++ KOSTASCM/src/main/java/com/example/KostascmApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KostascmApplication {
-<<<<<<< HEAD
-////
-=======
-
->>>>>>> branch 'master' of https://github.com/free8011/kosta_study.git
 	public static void main(String[] args) {
 		SpringApplication.run(KostascmApplication.class, args);
 	}
